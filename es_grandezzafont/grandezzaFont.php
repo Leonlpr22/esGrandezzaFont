a84c00b347b4bb1c542ea0355cb776421f7c5e77
Leonlpr22 <leonleone2005@gmail.com>
2024-09-24 11:35:44 +0200

    esercizio grandezza font

---
+++ es_grandezzafont/grandezzaFont.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        
+        $numcasuale = rand(16,60) . "px";
+        echo "<p style = 'font-size: $numcasuale;'>ciao prof</p>";
+
+        
+
+    ?>
+</body>
+</html>
